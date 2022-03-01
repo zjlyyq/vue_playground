4580766ecbc494e3161bfbd094fbd77903675db9
zjl <jialuzhang0805@gmail.com>
2022-03-01 15:28:54 +0800

    html解析:文本解析

--- vue_demo/compiler/parser/index.js
+++ vue_demo/compiler/parser/index.js
@@ -1,11 +1,13 @@
 let html = `<p class="greeting" id="xxx">Hello <span>World</span>!</p>`
 const ncname = '[a-zA-Z_][\\w\-\\.]*';  // \w 任意字母数字下划线
 const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+const startTagOpen = new RegExp(`^<${qnameCapture}`);
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+const comment = /^<!--/;
+// 条件注释 <![if IE 6]>
+const conditionalComment = /^<!\[/;
 
 function parseStartTag(template) {
-
-    const startTagOpen = new RegExp(`^<${qnameCapture}`);
-    
     const start = template.match(startTagOpen);
     
     const match = {
@@ -45,14 +47,12 @@ html = parseStartTag(html);
 
 // 解析结束标签
 function parseEndTag(html) {
-    const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
     const match = html.match(endTag);
     return  match;
 }
 
 // 解析注释
 function parseComment(html, options) {
-    const comment = /^<!--/;
     if (comment.test(html)) {
         const commentEnd = html.indexOf('-->');
         if (commentEnd > 0) {
@@ -63,8 +63,6 @@ function parseComment(html, options) {
             html = html.substring(commentEnd + 3);
         }
     }
-    // 条件注释 <![if IE 6]>
-    const conditionalComment = /^<!\[/;
     if (conditionalComment.test(html)) {
         const conditionalEnd = html.indexOf(']>');
         html = html.substring(conditionalEnd + 2)
@@ -72,7 +70,39 @@ function parseComment(html, options) {
 
 }
 
-parseEndTag('</div>')
+// 解析文本
+function parseText(html) {
+    while(html) {
+        let text = '', next, rest;
+        let textEnd = html.indexOf('<');
+        if (textEnd > -1) {
+            rest = html.slice(textEnd);
+            // 不符合任何被解析的类型
+            while( 
+                !endTag.test(html) &&
+                !startTagOpen.test(html) && 
+                !comment.test(html) && 
+                !conditionalComment.test(html)
+            ) {
+                next = rest.indexOf('<', 1);
+                if (next < 0) break;
+                textEnd += next;
+                rest = rest.slice(next);
+            }
+
+            text = html.substring(0, textEnd);
+            html = html.slice(textEnd);
+        }
+        if (textEnd < 0) {
+            text = html;
+            html = '';
+        } 
+
+        return text;
+    }
+}
+// parseEndTag('</div>')
+parseText('hello </div>');
 async function asyncFn() {
     const rand = Math.random();
     if (rand > 0.9) return true;
