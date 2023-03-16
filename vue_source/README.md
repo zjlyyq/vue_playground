fb1bf49c2bbf220a26c67ceabc2f5a988674163a
zjl <jialuzhang0805@gmail.com>
2023-03-16 15:30:31 +0800

    依赖收集在哪里part1

--- vue_source/README.md
+++ vue_source/README.md
@@ -2,4 +2,5 @@
 ### 变化侦测
 #### Object侦测
 1. 如何追踪变化 —— 使用Object.defineProperty
-2. 
+2. 如何收集依赖 —— 在getter中收集依赖，在setter中触发依赖
+3. 依赖收集在哪里
--- vue_source/defineReactive.js
+++ vue_source/defineReactive.js
@@ -5,15 +5,21 @@
  * @param {any} val 
  */
 function defineReactive(data, key, val) {
+  let dep = []; // 数组用于收集依赖
   Object.defineProperty(data, key, {
     enumerable: true,
     configurable: true,
     get: function () {
+      dep.push(window.target);
       return val;
     },
     set: function (newVal) {
       if (val === newVal) {
         return;
+      }
+      // 循环dep 以触发收集到的依赖。
+      for(let i = 0;i < dep.length; i++) {
+        dep[i](newVal. val);
       }
       val = newVal;
     }
